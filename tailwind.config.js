dcb29cb6a125dcaad57fa9c85ce2576bb843b1da
ben-wycliff <ben12wyclif@gmail.com>
2024-02-08 21:58:51 +0300

    Sets up tailwind in the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    './App.{js,jsx,ts,tsx}',
+    './src/screens/**/*.{js,jsx,ts,tsx}',
+    './src/components/**/*.{js,jsx,ts,tsx}',
+    "./screens/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
