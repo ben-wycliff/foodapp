9b4ed38b72b99246b8df2e25b367343683a64435
ben-wycliff <ben12wyclif@gmail.com>
2024-02-08 19:57:11 +0300

    Initial Commit

---
+++ App.js
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+// import 'tailwind-rn';
+// import React from 'react';
+// import {View, Text} from 'react-native';
+
+// const App = () => {
+//   return (
+//     <View className="flex-1 justify-center items-center bg-gray-100">
+//       <Text className="text-2xl font-bold text-blue-500">Hello, Tailwind!</Text>
+//     </View>
+//   );
+// };
+
+import AppNavigation from './src/navigation';
+
+function App(){
+  return (
+    <AppNavigation/>
+  );
+}
+export default App;
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import {View, Text} from 'react-native';
+
+
+function HomeScreen(){
+    return (
+        <View>
+            <Text>Home Screen</Text>
+            <Text>Music 2</Text>
+        </View>
+    );
+}
+
+export default HomeScreen;
