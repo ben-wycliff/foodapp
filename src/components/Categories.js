9d62b90e4c56c34c1c7b703df225c293e4548410
ben-wycliff <ben12wyclif@gmail.com>
2024-02-09 22:27:31 +0300

    Creates categories menu

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,12 +1,41 @@
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
-import { Text, View } from 'react-native';
+import { ScrollView, TouchableOpacity, View, Image, Text } from 'react-native';
+import {
+    widthPercentageToDP as wp,
+    heightPercentageToDP as hp,
+  } from 'react-native-responsive-screen';
+import { categoryData } from '../constants';
 
 
 export function Categories(props) {
 
   return (
     <View>
-        <Text>Categories</Text>
+        <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            className="space-x-4"
+            contentContainerStyle={{paddingHorizontal: 15}}>
+
+            {
+                categoryData.map( (category, index) => (
+                    <TouchableOpacity
+                        key={index}
+                        className="flex items-center space-y-1"
+                    >
+                        <View className="rounded-full p-[6px]">
+                            <Image 
+                                className="rounded-full"
+                                source={{uri: category.image}}
+                                style={{width: hp(6), height: hp(6)}}
+                            />
+                        </View>
+                        <Text className="text-neutral-600">{category.name}</Text>
+                    </TouchableOpacity>
+                ))
+            }
+            
+        </ScrollView>
     </View>
   );
 }
---
+++ src/constants/index.js
@@ -0,0 +1,33 @@
+export const categoryData = [
+  {name: 'asian', image: 'https://picsum.photos/200'},
+  {name: 'american', image: 'https://picsum.photos/200'},
+  {name: 'asian', image: 'https://picsum.photos/200'},
+  {name: 'breakfast', image: 'https://picsum.photos/200'},
+];
+
+export const mealData = [
+  {
+    name: 'meal 1',
+    image: 'https://picsum.photos/200',
+  },
+  {
+    name: 'meal 2',
+    image: 'https://picsum.photos/200',
+  },
+  {
+    name: 'meal 3',
+    image: 'https://picsum.photos/200',
+  },
+  {
+    name: 'meal 4',
+    image: 'https://picsum.photos/200',
+  },
+  {
+    name: 'meal 5',
+    image: 'https://picsum.photos/200',
+  },
+  {
+    name: 'meal 6',
+    image: 'https://picsum.photos/200',
+  },
+];
